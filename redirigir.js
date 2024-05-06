9ff18fbca7d3ae5ff8a3c0c1305b10ca143b8389
RodrigoSaporiti <rodrigosaporiti6949@gmail.com>
2024-05-05 23:54:42 -0300

    falta arreglar cosaas

---
+++ redirigir.js
@@ -0,0 +1,29 @@
+
+function user (){
+
+    window.location.href="user.html"
+
+}
+
+
+
+function carrito (){
+
+    window.location.href="carrito.html"
+
+}
+
+
+
+function fav (){
+
+    window.location.href="favoritos.html"
+
+}
+
+
+function inicio (){
+
+    window.location.href="index.html"
+
+}
