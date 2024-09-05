2d5950ba2ef127c7cc517344c83cec98f14fd9b2
RodrigoSaporiti <rodrigosaporiti6949@gmail.com>
2024-09-04 22:47:42 -0300

    index

---
+++ inicio.js
@@ -0,0 +1,16 @@
+// menu
+
+const menu = document.getElementById("menu");
+
+const asideMenu = document.getElementById("asideMenu");
+
+
+menu.addEventListener("click", ()=>{
+
+asideMenu.classList.toggle("d-none")
+
+
+
+})
+
+
