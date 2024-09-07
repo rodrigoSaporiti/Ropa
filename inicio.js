21af70ff96038f9e0d98e551a9fc179a082593ae
RodrigoSaporiti <rodrigosaporiti6949@gmail.com>
2024-09-07 01:52:13 -0300

    falta acomodar index

--- inicio.js
+++ inicio.js
@@ -4,11 +4,14 @@ const menu = document.getElementById("menu");
 
 const asideMenu = document.getElementById("asideMenu");
 
+const body = document.getElementById("body")
+
 
 menu.addEventListener("click", ()=>{
 
 asideMenu.classList.toggle("d-none")
 
+body.classList.toggle("overflow-hidden")
 
 
 })
